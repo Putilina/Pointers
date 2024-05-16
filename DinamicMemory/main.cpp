c88625c635c3d0c5995be1b3d6b42a34b10b29ab
Pascall <Pascall@HOME-PC>
2024-05-16 23:31:12 +0300

    Split

---
+++ DinamicMemory/main.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+
+#define   tab  "\t"
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	// new
+	int n = 5;
+	cout << "Введите размер массива:"; cin >> n;
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+
+	delete[] arr;
+	//Memory leaks
+}
+void FillRand(int arr[], const int n)
+{
+	
+       for (int i = 0; i < n; i++)
+	   {
+		*(arr + i) = rand() % 100;
+		//Обращение к элементам массива через арифметику указателей и оператор разыменования
+	   }
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+		//Обращение к элементам массива через оператор индексирования []
+	}
+	cout << endl;
+}
---
+++ Split/main.cpp
@@ -0,0 +1,70 @@
+﻿#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+
+	//1) Вычисляем размеры массивов для четных и нечетных элементов:
+	int even_count = 0;
+	int odd_count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)even_count++;
+		else odd_count++;
+	}
+	cout << "Количество четных элементов:" << even_count << endl;//number of even elements
+	cout << "Количество  нечетных элементов:" << odd_count << endl;//number of odd elements
+
+    //2) Выделяем память для массивов:
+	int* even_arr = new int[even_count];
+	int* odd_arr = new int[odd_count];
+
+	// 3) Выносим значения из исходного массива в соответствующие:
+
+	for (int i = 0, j = 0, k = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even_arr[j++] = arr[i];
+		}
+		else
+		{
+			odd_arr[k++] = arr[i];
+		}
+	}
+
+
+   // 4)  Выводим результаты на экран:
+
+   Print(even_arr, even_count);
+   Print(odd_arr, odd_count);
+
+// 5) !!!!!!!    УДАЛЯЕМ МАССИВЫ      !!!!!!!!!
+
+  delete[] odd_arr;
+  delete[] even_arr;
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i]=rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
